0ffc88c783f0057c8804013030af3fadd1df2191
kevintang6142 <kevintang6142@gmail.com>
2024-07-02 15:16:51 -0500

    Implement function to play a round

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let humanScore = 0, computerScore = 0;
+
 function getComputerChoice() {
     let r = Math.random() * 3;
     if (r < 1) {
@@ -24,5 +26,43 @@ function getPlayerChoice(message = "Rock, paper, or scissors? (r/p/s)") {
             break;
         default:
             return getPlayerChoice("Try again. Rock, paper, or scissors? (r/p/s)");
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Tie!");
+    }
+    else {
+        if (humanChoice === "Rock") {
+            if (computerChoice === "Scissors") {
+                console.log("You win! Rock beats scissors.");
+                humanScore++;
+            }
+            else {
+                console.log("You lose! Paper beats rock.");
+                computerScore++;
+            }
+        }
+        else if (humanChoice === "Paper") {
+            if (computerChoice === "Rock") {
+                console.log("You win! Paper beats rock.");
+                humanScore++;
+            }
+            else {
+                console.log("You lose! Scissors beats paper.");
+                computerScore++;
+            }
+        }
+        else {
+            if (computerChoice === "Paper") {
+                console.log("You win! Scissors beats paper.");
+                humanScore++;
+            }
+            else {
+                console.log("You lose! Rock beats scissors.");
+                computerScore++;
+            }
+        }
     }
 }
